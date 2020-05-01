917b01e49a4c2c9eff90dbfe734ece973373aacd
J-A-Cgyt <764091925@qq.com>
2020-05-01 15:27:09 +0800

    非递归的最大子数组求解

--- 程设01/minSub_Array.cpp
+++ 程设01/minSub_Array.cpp
@@ -7,6 +7,8 @@ class MinSubArray_Info
 public:
 	void showInfo();
 	void SetInfo(int min,int max,int sum);
+	int GetMin();
+	int GetMax();
 private:
 	int minLabel;
 	int maxLabel;
@@ -26,6 +28,16 @@ void MinSubArray_Info::SetInfo(int min, int max, int sum)
 	this->maxLabel = max;
 	this->minLabel = min;
 	this->Sum = sum;
+}
+
+int MinSubArray_Info::GetMin()
+{
+	return minLabel;
+}
+
+int MinSubArray_Info::GetMax()
+{
+	return maxLabel;
 }
 
 MinSubArray_Info FindSubarrayCross(int *ArrayPt, int low,int mid,int high)
@@ -103,29 +115,35 @@ MinSubArray_Info FindMaxSubArray(int* ArrayPt, int low, int high)
 int main()
 {
 	int A[10];
-	A[0] = 3; A[1] = -2; A[2] = 9; A[3] = -1; A[4] = 2; A[5] = 7; A[6] = -12; A[7] = 13; A[8] = -6; A[9] = 1;
+	//A[0] = 3; A[1] = -2; A[2] = 9; A[3] = -1; A[4] = 2; A[5] = 7; A[6] = -12; A[7] = 13; A[8] = -6; A[9] = 1;
+	A[0] = 3; A[1] = 6; A[2] = -10; A[3] = 1; A[4] = 2; A[5] = 7; A[6] = -8; A[7] = 13; A[8] = -6; A[9] = 1;
 	MinSubArray_Info res;
 	res = FindMaxSubArray(A, 0, 9);
 	res.showInfo();
 
 	//非递归的方法尝试
-	MinSubArray_Info Res_2;
+	MinSubArray_Info Res_2, Border;
 	Res_2.SetInfo(0, 0, A[0]);
+	Border.SetInfo(0, 0, A[0]);
 	int sum_temp = 0;
 	int sum = 0;
-	for (int i = 0; i < 10; i++)
+	/*线性方法实现20200501，注意条件比较和等效，以及已经计算过的条件的复用！
+	和CSDN昨晚看见的文章吻合——利用之前的计算结果进行判定并借此减少工作量。
+	对进行算法分析而言，用规模较小的问题进行试验始终是一个较好的思路来源
+	*/for (int i = 1; i < 10; i++)
 	{
-		for (int j = 0; j < i; j++)
+		if (Border.Sum + A[i] < A[i])
 		{
-			for (int k = j; k < i; k++)
-			{
-				sum_temp = sum_temp + A[j];
-			}
-			if (sum < sum_temp)
-			{
-				sum = sum_temp;
-				Res_2.SetInfo(j, i, sum);
-			}
+			Border.SetInfo(i, i, A[i]);
+		}
+		else 
+		{
+			Border.SetInfo(Border.GetMin(), i, Border.Sum + A[i]);
+		}
+
+		if (Border.Sum > Res_2.Sum)
+		{
+			Res_2.SetInfo(Border.GetMin(), Border.GetMax(), Border.Sum);
 		}
 	}
 
