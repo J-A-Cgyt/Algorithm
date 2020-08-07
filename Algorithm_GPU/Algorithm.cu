f66dcfd68db4684c0a0d70f15378ba8effcec36d
764091925 <764091925@qq.com>
2020-08-07 20:55:14 +0800

    拉取算法试验田并新建分支

---
+++ Algorithm_GPU/Algorithm.cu
@@ -0,0 +1,1 @@
+#include "Head_G.cuh"
---
+++ Algorithm_GPU/Head_G.cuh
@@ -0,0 +1,10 @@
+//cuda运行时
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+//标准输出
+#include <stdio.h>
+
+//opencv
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
